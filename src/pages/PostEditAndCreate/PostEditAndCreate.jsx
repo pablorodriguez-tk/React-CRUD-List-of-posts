add55f8cf95bdedc3c0db0175d09855657b3d8f7
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-04-09 18:05:20 -0300

    Add Post and Create Form

--- src/pages/PostEditAndCreate/PostEditAndCreate.jsx
+++ src/pages/PostEditAndCreate/PostEditAndCreate.jsx
@@ -1,5 +1,97 @@
-const PostEditAndCreate = () => {
-  return <div>Post Edit And Create</div>;
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import EditIcon from "@material-ui/icons/Edit";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const PostEditAndCreate = (props) => {
+  const classes = useStyles();
+  console.log(props);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <EditIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {props.match.params.postId === "create" ? "Create Post" : "Edit Post"}
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="Post Title"
+            label="Post Title"
+            name="Post Title"
+            autoFocus
+            type="text"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="Post Details"
+            label="Post Details"
+            type="text"
+            id="Post Details"
+            multiline={true}
+          />
+          <Grid container justify="space-between">
+            <Grid item xs={3}>
+              <Button className={classes.submit} variant="contained">
+                Go Back
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default PostEditAndCreate;
